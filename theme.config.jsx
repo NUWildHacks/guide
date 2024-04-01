36e65ce50d038e1c34ab84e19a44fde8877c36e7
Dilan <dilan@dilanxd.com>
2024-04-01 01:25:39 -0500

    set up guide

---
+++ theme.config.jsx
@@ -0,0 +1,19 @@
+export default {
+  logo: (
+    <div
+      style={{
+        display: 'flex',
+        gap: '8px',
+        alignItems: 'center',
+        fontSize: '20px',
+        fontWeight: '500',
+      }}
+    >
+      <img src="/wildhacks2024.svg" style={{ height: '32px' }} />
+      <span>WildHacks 2024 Guide</span>
+    </div>
+  ),
+  darkMode: false,
+  primaryHue: 0,
+  primarySaturation: 98.57142857142858,
+};
